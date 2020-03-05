6f2b346cc50582e0e071fcb75b4073de74b602c6
Lewis Nelson <lewis.nelson@iohk.io>
2020-03-05 14:49:06 +0000

    Fixed create-pages script

--- scripts/create-pages.js
+++ scripts/create-pages.js
@@ -4,13 +4,25 @@ const config = require('../site.config.json')
 
 const paths = process.argv.slice(2)
 
+function createDirectory (filePath) {
+  const leadingDirectory = filePath.split(path.sep)
+  leadingDirectory.pop()
+  let nextPath
+  leadingDirectory.forEach(segment => {
+    nextPath = nextPath ? path.join(nextPath, segment) : `${path.sep}${segment}`
+    if (!fs.existsSync(nextPath) || !fs.statSync(nextPath).isDirectory) {
+      fs.mkdirSync(nextPath)
+    }
+  })
+}
+
 function getResolvedPath (normalizedPath, rootName = 'index') {
   let resolvedPath, resolvedName
   if (!normalizedPath) {
     resolvedName = rootName
     resolvedPath = resolvedName
   } else {
-    resolvedName = normalizedPath.splt('/').pop()
+    resolvedName = normalizedPath.split('/').pop()
     resolvedPath = path.join(...normalizedPath.split('/'))
   }
 
@@ -20,11 +32,10 @@ function getResolvedPath (normalizedPath, rootName = 'index') {
 function createNetlifyCollection (normalizedPath) {
   const [ markdownRelativePath ] = getResolvedPath(normalizedPath)
   const [ resolvedPath, name ] = getResolvedPath(normalizedPath, 'home')
-  const jsContent = `
-export default {
-  name: '${normalizedPath || name}_page',
+  const jsContent = `export default {
+  name: '${(normalizedPath && normalizedPath.split('/').join('_')) || name}_page',
   label: '${name} page',
-  folder: '${markdownRelativePath}',
+  folder: 'resources/content/${markdownRelativePath}',
   create: false,
   delete: false,
   fields: [
@@ -47,8 +58,7 @@ export default {
     }
   ]
 }
-
-  `
+`
 
   const fullPath = path.join(__dirname, '..', 'netlify', 'collections', 'pages', `${resolvedPath}.js`)
   if (fs.existsSync(fullPath) && fs.statSync(fullPath).isFile) {
@@ -56,25 +66,26 @@ export default {
     return
   }
 
+  createDirectory(fullPath)
   fs.writeFileSync(fullPath, jsContent, { encoding: 'utf8' })
   console.log(`Netlify collection created ${resolvedPath}.js`)
 
   const indexPath = path.join(__dirname, '..', 'netlify', 'collections', 'pages', 'index.js')
   const indexContent = fs.readFileSync(indexPath, { encoding: 'utf8' })
-  indexContent
-    .replace(/(import\s[^\s]+\sfrom\s'[^']+'\n)/, `$1import ${name.replace('-', '')} from './${resolvedPath}.js'\n`)
-    .replace(/(export\sdefault\s\[)([^\]]+)(\])/, (matches) => {
-      return `${matches[1]}${matches[2].replace(/\n$/, `\n  ${name.replace('-', '')}\n`)}`
+  const newIndexContent = indexContent
+    .replace(/(import\s[^\s]+\sfrom\s'[^']+'\n)/, `$1import ${resolvedPath.replace(path.sep, '')} from './${resolvedPath.split(path.sep).join('/')}'\n`)
+    .replace(/(export\sdefault\s\[)([^\]]+)(\])/, (_, a, b, c) => {
+      return `${a}${b.replace(/\n$/, `,\n  ${resolvedPath.replace(path.sep, '')}\n`)}${c}`
     })
 
-  fs.writeFileSync(indexPath, indexContent, { encoding: 'utf8' })
+  fs.writeFileSync(indexPath, newIndexContent, { encoding: 'utf8' })
   console.log('Netlify collection index updated')
 }
 
 function getQueryName (normalizedPath) {
   if (!normalizedPath) return 'Index'
   return normalizedPath.split('/').map(part => {
-    return part.split('-').map(word => `${word[0].toUpperCase}${word.split('').slice(1).join('').toLowerCase()}`).join('-')
+    return part.split('-').map(word => `${word[0].toUpperCase()}${word.split('').slice(1).join('').toLowerCase()}`).join('-')
   }).join('')
 }
 
@@ -83,20 +94,20 @@ function createMarkdown (normalizedPath) {
   const [ markdownRelativePath, markdownName ] = getResolvedPath(normalizedPath)
 
   config.availableLanguages.forEach(language => {
-    const markdownFileRelativePath = path.join(contentRelativePath, markdownRelativePath, `${markdownName}-${language.key}.md`)
+    const markdownFileRelativePath = path.join(contentRelativePath, markdownRelativePath, `${markdownRelativePath.split(path.sep).join('-')}-${language.key}.md`)
     const markdownFilePath = path.join(__dirname, '..', markdownFileRelativePath)
-    const markdown = `
----
+    const markdown = `---
 title: ${language.key} (${language.label}) ${markdownName} page content
 content:
   default_content: |
     This page was created using npm run create-pages. This content can be edited in ${markdownFileRelativePath}. The query can be updated in src/queries/${getQueryName(normalizedPath)}Page.js. The Netlify collection can be updated in netlify/collections/pages/${markdownRelativePath}.js.
 ---    
-    `
+`
 
     if (fs.existsSync(markdownFilePath) && fs.statSync(markdownFilePath).isFile) {
       console.log(`Markdown file for page ${markdownFileRelativePath} already exists`)
     } else {
+      createDirectory(markdownFilePath)
       fs.writeFileSync(markdownFilePath, markdown, { encoding: 'utf8' })
       console.log(`Markdown file created ${markdownFileRelativePath}`)
     }
@@ -105,21 +116,20 @@ content:
 
 function createQuery (normalizedPath) {
   const queryName = getQueryName(normalizedPath)
-  const queryRelativePath = path.join('src', 'queries', `${queryName}.js`)
-  const [ markdownRelativePath, markdownName ] = getResolvedPath(normalizedPath)
-  const queryContent = `
-import React from 'react'
+  const queryRelativePath = path.join('src', 'queries', `${queryName}PageQuery.js`)
+  const [ markdownRelativePath ] = getResolvedPath(normalizedPath)
+  const queryContent = `import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 import Language from '@input-output-hk/front-end-core-components/components/Language'
 
-const ${queryName} = ({ render }) => (
+const ${queryName}PageQuery = ({ render }) => (
   <Language.Consumer>
     {({ key: lang }) => (
       <StaticQuery
         query={graphql\`
           query {
-            allFile(filter:{relativePath:{glob:"${markdownRelativePath}/*.md"}}) {
+            allFile(filter:{relativePath:{glob:"content/pages/${markdownRelativePath}/*.md"}}) {
               nodes{
                 relativePath,
                 childMarkdownRemark{
@@ -134,8 +144,8 @@ const ${queryName} = ({ render }) => (
           }
         \`}
         render={({ allFile }) => {
-          const content = allFile.nodes.filter(node => node.relativePath === \`${markdownRelativePath}/${markdownName}-\${lang}.md\`).shift()
-          if (!content || !content.childMarkdownRemark) throw new Error(\`No ${markdownName} translations found for language \${lang}\`)
+          const content = allFile.nodes.filter(node => node.relativePath === \`content/pages/${markdownRelativePath}/${markdownRelativePath.split(path.sep).join('-')}-\${lang}.md\`).shift()
+          if (!content || !content.childMarkdownRemark) throw new Error(\`No ${markdownRelativePath.split(path.sep).join('-')} translations found for language \${lang}\`)
           return render(content.childMarkdownRemark.frontmatter.content)
         }}
       />
@@ -143,13 +153,12 @@ const ${queryName} = ({ render }) => (
   </Language.Consumer>
 )
 
-${queryName}.propTypes = {
+${queryName}PageQuery.propTypes = {
   render: PropTypes.func.isRequired
 }
 
-export default ${queryName}
-
-  `
+export default ${queryName}PageQuery
+`
 
   const fullPath = path.join(__dirname, '..', queryRelativePath)
   if (fs.existsSync(queryRelativePath) && fs.statSync(queryRelativePath).isFile) {
@@ -165,16 +174,15 @@ function createPage (normalizedPath) {
   const queryName = getQueryName(normalizedPath)
   const [ pageRelativePath ] = getResolvedPath(normalizedPath)
   const previousDirectories = normalizedPath.split('/').map(() => '..').join('/')
-  const pageContent = `
-import React from 'react'
+  const pageContent = `import React from 'react'
 import Box from '@material-ui/core/Box'
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
 import Layout from '${previousDirectories}/components/Layout'
-import ${queryName} from '${previousDirectories}/queries/${queryName}'
+import ${queryName}PageQuery from '${previousDirectories}/queries/${queryName}PageQuery'
 
 export default () => (
-  <AboutPageQuery
+  <${queryName}PageQuery
     render={(content) => (
       <Layout>
         <Container maxWidth='lg'>
@@ -190,8 +198,7 @@ export default () => (
     )}
   />
 )
-
-  `
+`
 
   const fullPath = path.join(__dirname, '..', 'src', 'pages', `${pageRelativePath}.js`)
   if (fs.existsSync(fullPath) && fs.statSync(fullPath).isFile) {
@@ -199,6 +206,7 @@ export default () => (
     return
   }
 
+  createDirectory(fullPath)
   fs.writeFileSync(fullPath, pageContent, { encoding: 'utf8' })
   console.log(`Page created ${pageRelativePath}.js`)
 }
